7fc0f2f573f0a55de6a6e73c8969d5e952e8ee08
LaiaPomar <laiapomar12@gmail.com>
2021-10-22 23:04:27 +0200

    Fix percentatges

--- src/PercentatgePreu.java
+++ src/PercentatgePreu.java
@@ -1,17 +1,17 @@
 public class PercentatgePreu {
-    static public int getGuanyat(int n) {
+    static public double getGuanyat(int n) {
         if (n == 0) {
-            return 102;
+            return 1.02;
         }
         // Percentatge guanyat = 100 - 2^n
-        return 100 - (1 << n);
+        return (double)(100 - (1 << n))/100;
     }
 
-    static public int getPerdut(int n) {
+    static public double getPerdut(int n) {
         if (n == 0) {
-            return 4;
+            return 0.04;
         }
         // Percentatge perdut = (100 - 2^n) - (100 - 2^(n+1)) = 2^n
-        return 1 << n;
+        return (double)(1 << n)/100;
     }
 }
