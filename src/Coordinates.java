bc63d2236085e5d291c6bf3fd9404d775b1ebbca
EdgarFIB <edgar.moreno.martinez@estudiantat.upc.edu>
2021-10-31 18:50:46 +0100

    Clean

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -26,14 +26,6 @@ public class Coordinates {
         return y;
     }
 
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
