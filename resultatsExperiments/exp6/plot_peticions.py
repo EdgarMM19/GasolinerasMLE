42e79bdf80af1a2b4fcc28e3958601a72e59820e
LaiaPomar <laiapomar12@gmail.com>
2021-11-01 01:47:01 +0100

    Experiments 6, 7 i 8

---
+++ resultatsExperiments/exp6/plot_peticions.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = []
+cost = [2,4,8,16,32,64,128]
+
+filename = f"./num_peticions.csv"
+with open(filename, "r") as f:
+    data = [float(x) for x in f.readlines()]
+
+plt.xlabel('Cost de recòrrer un quilòmetre')
+plt.ylabel('Número de peticions servides')
+plt.plot(cost, data, label = "Cost quilòmetre vs peticions ateses", marker = "o")
+plt.savefig("dotplot-exp6-peticions.png", dpi=200)
+plt.show()
